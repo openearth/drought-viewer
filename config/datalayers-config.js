544ab767200359df194c635ab4de1d6210f2fa12
langelova <urb.lili.an@gmail.com>
2020-08-12 13:52:21 +0200

    update

--- config/datalayers-config.js
+++ config/datalayers-config.js
@@ -7,7 +7,6 @@ const tab4 = "Tijdreeksen grondwaterstanden";
 
 
 const items_tab2 = [
-
       { id: 'Zomergrondwaterstand droog', name: 'Zomergrondwaterstand droog' , layer: 'scenario_DROOG_gwl_diff2ref_month01-2020-07-22T00:00:00Z', time_stamp: '2020-05-01T00:00:00.000Z'},
       { id: 'Zomergrondwaterstand gemiddeld', name: 'Zomergrondwaterstand gemiddeld' , layer: 'scenario_DROOG_gwl_diff2ref_month01-2020-07-22T00:00:00Z', time_stamp: '2020-06-01T00:00:00.000Z'},
       { id: 'Zomergrondwaterstand net', name: 'Zomergrondwaterstand net' , layer: 'scenario_DROOG_gwl_diff2ref_month01-2020-07-22T00:00:00Z', time_stamp: '2020-08-01T00:00:00.000Z'}
@@ -15,23 +14,9 @@ const items_tab2 = [
   ;
 
 const items_tab3 = [
-  // {
-  //   id: 1,
-  //   name: 'Leaf Area Index',
-  //   children: [
-  //     { id: 'LAI00000_01', name:'LAI00000 01', layer: 'rhine:LAI00000_01' },
-  //     { id: 'LAI00000_02', name:'LAI00000 02', layer: 'rhine:LAI00000_02' },
-  //     { id: 'LAI00000_03', name:'LAI00000 03', layer: 'rhine:LAI00000_03' },
-  //     { id: 'LAI00000_04', name:'LAI00000 04', layer: 'rhine:LAI00000_04' },
-  //     { id: 'LAI00000_05', name:'LAI00000 05', layer: 'rhine:LAI00000_05' },
-  //     { id: 'LAI00000_06', name:'LAI00000 06', layer: 'rhine:LAI00000_06' },
-  //     { id: 'LAI00000_07', name:'LAI00000 07', layer: 'rhine:LAI00000_08' },
-  //     { id: 'LAI00000_09', name:'LAI00000 09', layer: 'rhine:LAI00000_09' },
-  //     { id: 'LAI00000_10', name:'LAI00000 10', layer: 'rhine:LAI00000_10' },
-  //     { id: 'LAI00000_11', name:'LAI00000 11', layer: 'rhine:LAI00000_11' },
-  //     { id: 'LAI00000_12', name:'LAI00000 12', layer: 'rhine:LAI00000_12' },
-  //   ],
-  // }
+  { id: 'Scenario droog', name: 'Scenario droog' , layer: '', time_stamp: ''},
+  { id: 'Scenario gemiddeld', name: 'Scenario gemiddeld' , layer: '', time_stamp: ''},
+  { id: 'Scenario net', name: 'Scenario net' , layer: '', time_stamp: ''}
 ];
 
 export { app_name, tab1, tab2, tab3, tab4, items_tab2, items_tab3 };
--- src/components/app-shell/welcome-dialog.vue
+++ src/components/app-shell/welcome-dialog.vue
@@ -1,51 +1,37 @@
 <template>
-
   <v-dialog
     max-width="700px"
     v-model="showWelcomeDialog">
 
-   <v-card
-   max-width="700px"
-   >
+    <v-card light max-width="700px">
 
-      <v-card-title></v-card-title>
-      <v-spacer
-        class="title text-center"
-      >
+      <v-card-title>
+        <span class="headline">
+          Disclaimer:
+        </span>
+      </v-card-title>
 
-       DISCLAIMER:
-      </v-spacer>
-      <v-spacer
-        class="title text-center"
-      >
-       This viewer is generated with the purpose of informing the general public....
-      </v-spacer>
-      <v-card-title></v-card-title>
+      <v-card-text>
+        This viewer is generated with the purpose of informing the general public....
+      </v-card-text>
 
-      <v-layout align-end justify-center>
-          <v-flex xs6>
-            <!--- v-flex is centered now, but we need to center button inside v-flex -->
-            <div class="text-center">
-              <v-btn
-               class="primary "
-               text
-               @click="onStartClick"
-               >Let's get started
-
-               </v-btn>
-              <v-card-title></v-card-title>
-            </div>
-          </v-flex>
-      </v-layout>
+      <v-card-actions>
+        <v-row justify="center" class="pa-0 ma-0">
+             <v-btn
+                class="primary "
+                text
+                @click="onStartClick"
+                >Accepteer en naar website
+              </v-btn>
+          <!-- <v-btn tutorialDialog=true" round color="primary" dark>Decline</v-btn> -->
+        </v-row>
+      </v-card-actions>
     </v-card>
   </v-dialog>
-
 </template>
 
 <script>
 const LOCALSTORAGE_KEY = 'intro_seen';
-
-
 export default {
   data: () => ({
     showWelcomeDialog: true

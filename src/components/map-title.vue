88cd36125c0e1a50f025d870e942b61826f604f5
IoannaMi1 <ioannamicha@hotmail.com>
2022-03-18 17:25:23 +0100

    Add map title component

---
+++ src/components/map-title.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card class="map-title" flat>
+    <p class="map-title-text text-body-2">
+      {{ title }}
+    </p>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .map-title {
+    position: absolute;
+    z-index: 1;
+    top: 14px;
+    left: 14px;
+    padding: 8px 12px;
+    background-color: #ffffff;
+    user-select: none;
+  }
+
+  .map-title .map-title-text {
+    margin: 0;
+  }
+</style>
+ 

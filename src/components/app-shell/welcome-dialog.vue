1cfda1047b7ab4aa7a2598dcb4216feb222e85ea
Ilja America <ilja.america@deltares.nl>
2023-01-20 16:32:30 +0100

    check welcome dialog

--- src/components/app-shell/welcome-dialog.vue
+++ src/components/app-shell/welcome-dialog.vue
@@ -20,7 +20,12 @@
         Doel is om de prognoses samen met partners verder te ontwikkelen tot een nationaal droogtevoorspellingssysteem dat zowel waterbeheerders als watergebruikers van waardevolle informatie voorziet om tijdig droogtemaatregelen in te kunnen zetten.
       </v-card-text>
       <v-card-text>
-        De website is met de grootst mogelijke zorgvuldigheid opgezet. Echter, de website dient slechts voor informatieve doeleinden. Er kunnen geen rechten aan de informatie op de website worden ontleend en het gebruik van de website en de aangeboden informatie is voor uw eigen rekening en risico. Deltares aanvaart geen enkele vorm van aansprakelijkheid - uit welke hoofde dan ook - voor enige vorm van schade die op enigerlei wijze voortvloeit uit de werking van en/of het gebruik van (informatie op) de website. Deltares kan niet garanderen dat de website virusvrij is of zonder fouten en/of onderbrekingen zal functioneren. De website kan te allen tijde worden gewijzigd door Deltares. Op de website alsmede op deze voorwaarden is Nederlands recht van toepassing.
+        De website is met de grootst mogelijke zorgvuldigheid opgezet. Echter, de website dient slechts voor informatieve doeleinden.
+        Er kunnen geen rechten aan de informatie op de website worden ontleend en het gebruik van de website en de aangeboden informatie is voor uw eigen rekening en risico.
+        Deltares aanvaart geen enkele vorm van aansprakelijkheid - uit welke hoofde dan ook - voor enige vorm van schade die op enigerlei wijze voortvloeit uit de werking van en/of het gebruik van (informatie op) de website.
+        Deltares kan niet garanderen dat de website virusvrij is of zonder fouten en/of onderbrekingen zal functioneren.
+        De website kan te allen tijde worden gewijzigd door Deltares.
+        Op de website alsmede op deze voorwaarden is Nederlands recht van toepassing.
       </v-card-text>
 
       <v-card-actions>
